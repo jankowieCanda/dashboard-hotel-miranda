a336f75dd9b0afc0f0b068f7175fdaf24b955e8a
Jankowiec <ghjankowiec@gmail.com>
2024-05-09 20:15:35 +0200

    ADD: types Users

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,35 +1,33 @@
-import { useDispatch, useSelector } from "react-redux";
 import { Data_Table } from "../components/Data_Table";
 import { Header } from "../components/Header";
 import { SideNav } from "../components/SideNav";
-import { getAllUsers, getError, getStatus, getUser } from "../features/users/usersSlice";
+import { getAllUsers } from "../features/users/usersSlice";
 import { fecthAllUsersThunk } from "../features/users/usersThunk";
 import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { User } from "../interfaces/UsersInterfaces";
 
 export const Users = () => {
-    const dispatch = useDispatch();
-    const allUsers = useSelector(getAllUsers);
-    const user = useSelector(getUser);
-    const status = useSelector(getStatus);
-    const error = useSelector(getError);
+    const dispatch = useAppDispatch();
+    const allUsers: User[] = useAppSelector(getAllUsers);
 
     useEffect(() => {
         dispatch(fecthAllUsersThunk());
     }, [])
     
     
-    const cols = [
-        {property: '', display: data => (<img src={data.Picture} alt="picture"/>)},
-        {property: 'Full Name', display: data => data.Full_Name},
-        {property: 'Employee ID', display: data => data.Employee_ID},
-        {property: 'Email', display: data => data.Email},
-        {property: 'Start Date', display: data => data.Start_Date},
-        {property: 'Description', display: data => data.Description},
-        {property: 'Contact', display: data => data.Contact},
-        {property: 'Status', display: data => data.Status}
+    const cols: Object[] = [
+        {property: '', display: (data: User) => (<img src={data.Picture} alt="picture"/>)},
+        {property: 'Full Name', display: (data: User) => data.Full_Name},
+        {property: 'Employee ID', display: (data: User) => data.Employee_ID},
+        {property: 'Email', display: (data: User) => data.Email},
+        {property: 'Start Date', display: (data: User) => data.Start_Date},
+        {property: 'Description', display: (data: User) => data.Description},
+        {property: 'Contact', display: (data: User) => data.Contact},
+        {property: 'Status', display: (data: User) => data.Status}
     ];
 
-    const tabs = [
+    const tabs: Object[] = [
         {label: 'All Employees'},
         {label: 'Active Employee'},
         {label: 'Inactive Employee'},
